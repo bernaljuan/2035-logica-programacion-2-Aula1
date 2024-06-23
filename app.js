b3ba485a77357dd63b77ca4d2031dfc6bff239e9
Juan Bernal <bernaljuankrlos@gmail.com>
2024-06-23 09:37:21 -0500

    Segundo commit

--- app.js
+++ app.js
@@ -1,1 +1,34 @@
-document.querySelector('h1');
+let numeroSecreto = generarNumeroSecreto();
+let intentos = 1;
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto;
+    return;
+}
+
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value);
+    console.log(intentos);
+    if (numeroDeUsuario === numeroSecreto) {
+        asignarTextoElemento('p', 'Acertaste');
+    } else {
+        if (numeroDeUsuario > numeroSecreto) {
+            asignarTextoElemento('p', 'el numero es menor');
+        } else {
+            asignarTextoElemento('p', 'el numero es mayor');
+        }
+        intentos++;
+    }
+    return;
+}
+
+function generarNumeroSecreto() {
+    return Math.floor(Math.random() * 10)+1;
+    
+}
+
+asignarTextoElemento('h1','Juego del numero secreto!');
+asignarTextoElemento('p','Adivina el número entre 1 y 10');
+
+
